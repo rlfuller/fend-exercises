b951a6fe34315cbf1268a80038ec492dd33e68bd
Rachel Fuller <rlfuller@gmail.com>
2018-11-09 20:28:54 -0500

    First revision - requirements changed

--- cat-clicker/js/main.js
+++ cat-clicker/js/main.js
@@ -1,8 +1,23 @@
-let catEl = document.getElementById("cat-photo");
-let clickTotalEl = document.getElementById("click-total");
-let clickTotal = 0;
+let cat1El = document.getElementById("cat-photo1");
+let cat2El = document.getElementById("cat-photo2");
+let clickTotalEl1 = document.getElementById("click-total-cat1");
+let clickTotalEl2 = document.getElementById("click-total-cat2");
+let clickTotal1 = 0;
+let clickTotal2 = 0;
+const cat1Name = "Caticus Aurealis";
+const cat2Name = "Judd";
 
-catEl.addEventListener("click", function(){
-    clickTotal += 1;
-    clickTotalEl.innerText = "You have clicked the cat " + clickTotal + " time(s)";
+let cat1NameEl = document.getElementById("cat1name");
+let cat2NameEl = document.getElementById("cat2name");
+cat1NameEl.innerText = cat1Name;
+cat2NameEl.innerText = cat2Name;
+
+cat1El.addEventListener("click", function(){
+    clickTotal1 += 1;
+    clickTotalEl1.innerText = "You have clicked the cat " + clickTotal1 + " time(s)";
+}, false);
+
+cat2El.addEventListener("click", function(){
+    clickTotal2 += 1;
+    clickTotalEl2.innerText = "You have clicked the cat " + clickTotal2 + " time(s)";
 }, false);
