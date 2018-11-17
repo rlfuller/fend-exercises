988c718b81eef757ff81966b0b0060421a9c600d
Rachel Fuller <rlfuller@gmail.com>
2018-11-17 14:41:33 -0500

    small style improvements

--- cat-clicker/js/main.js
+++ cat-clicker/js/main.js
@@ -1,41 +1,18 @@
-// let cat1El = document.getElementById("cat-photo1");
-// let cat2El = document.getElementById("cat-photo2");
-// let clickTotalEl1 = document.getElementById("click-total-cat1");
-// let clickTotalEl2 = document.getElementById("click-total-cat2");
-// let clickTotal1 = 0;
-// let clickTotal2 = 0;
-// const cat1Name = "Caticus Aurealis";
-// const cat2Name = "Judd";
-
-// let cat1NameEl = document.getElementById("cat1name");
-// let cat2NameEl = document.getElementById("cat2name");
-// cat1NameEl.innerText = cat1Name;
-// cat2NameEl.innerText = cat2Name;
-
-// cat1El.addEventListener("click", function(){
-//     clickTotal1 += 1;
-//     clickTotalEl1.innerText = "You have clicked the cat " + clickTotal1 + " time(s)";
-// }, false);
-
-// cat2El.addEventListener("click", function(){
-//     clickTotal2 += 1;
-//     clickTotalEl2.innerText = "You have clicked the cat " + clickTotal2 + " time(s)";
-// }, false);
 
 let numClicks = 0;
 let $list = document.querySelector("#cat-list");
-let $catFragment = document.createDocumentFragment();
-// let $img = document.querySelector("img");
+//let $catFragment = document.createDocumentFragment();
+
 let $catName = document.querySelector("#name");
 let $totalCatClicks = document.querySelector("p");
 let $mainPanel = document.querySelector("#main");
 
-class Cat {
-    constructor(name){
-        this.name = name;
-        this.numClicks = 0;
-    }
-}
+// class Cat {
+//     constructor(name){
+//         this.name = name;
+//         this.numClicks = 0;
+//     }
+// }
 
 let cats = [
     {name: "dora", path: "../images/dora.jpg", clicks: 0},
@@ -61,7 +38,7 @@ cats.forEach(function(cat){
         $totalCatClicks.textContent = `This cat has been clicked ${cat.clicks} time(s)`;
     });
 
-    $mainPanel.appendChild($img);
+    $mainPanel.appendChild($img);   
 
     let $li = document.createElement("li");
     $li.appendChild($anchor);
@@ -71,7 +48,7 @@ cats.forEach(function(cat){
         
         $catName.textContent = cat.name;
 
-        document.querySelectorAll("img").forEach
+        //document.querySelectorAll("img").forEach
         let images = Array.from(document.querySelectorAll("img")).forEach(function(image){
             image.className = "hidden";
             $totalCatClicks.textContent = `This cat has been clicked ${cat.clicks} time(s)`;
